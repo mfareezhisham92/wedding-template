6a05887b7b8444137c59dedc23478145605f2de7
mfareezhisham92 <mfareezhisham92@gmail.com>
2026-06-30 13:53:42 +0800

    Update components.js

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,51 +1,29 @@
 const Components = {
-
   moment(content){
-
     return `
       <section class="moment active">
-
         ${content}
-
       </section>
     `;
-
   },
 
   label(text){
-
     if(!text) return "";
-
-    return `
-      <div class="label">
-        ${text}
-      </div>
-    `;
-
+    return `<div class="label">${text}</div>`;
   },
 
   title(text){
-
     if(!text) return "";
-
-    return `
-      <h1>
-        ${text}
-      </h1>
-    `;
-
+    return `<h1>${text}</h1>`;
   },
 
   paragraph(text){
-
     if(!text) return "";
+    return `<p>${text}</p>`;
+  },
 
-    return `
-      <p>
-        ${text}
-      </p>
-    `;
-
+  photo(src){
+    if(!src) return "";
+    return `<img src="${src}" alt="Memory photo">`;
   }
-
 };
