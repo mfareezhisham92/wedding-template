9032b3f2c3b63dbadc17258467e5eb7dc79977b3
mfareezhisham92 <mfareezhisham92@gmail.com>
2026-07-08 12:19:12 +0800

    Update director.js

--- assets/js/director.js
+++ assets/js/director.js
@@ -6,7 +6,18 @@ const Director = {
     hopeful: HOPE
   },
 
+  blueprints: {
+    romantic: EXPRESS_BLUEPRINT,
+    nostalgic: EXPRESS_BLUEPRINT,
+    calm: EXPRESS_BLUEPRINT,
+    hopeful: EXPRESS_BLUEPRINT
+  },
+
   getJourney(type){
     return this.journeys[type] || EXPRESS;
+  },
+
+  getBlueprint(type){
+    return this.blueprints[type] || EXPRESS_BLUEPRINT;
   }
 };
