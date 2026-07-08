4e0316e9ba6dfe2b38da16f4f37dfd431cf5733a
mfareezhisham92 <mfareezhisham92@gmail.com>
2026-07-08 11:52:43 +0800

    Create royal.js

---
+++ assets/js/collections/royal.js
@@ -0,0 +1,12 @@
+const ROYAL = {
+  id: "royal",
+  name: "Royal Collection",
+
+  style: {
+    typography: "classic",
+    atmosphere: "gold",
+    photoStyle: "cinematic",
+    transitions: "slow",
+    music: "warm-piano"
+  }
+};
